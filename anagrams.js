92bdcc5d6f326f09cf4a10732df7cd4dff3d6033
victorvoermans <voermans.v.m@gmail.com>
2025-01-25 11:52:01 +0100

    Cleanup

--- anagrams.js
+++ anagrams.js
@@ -1,26 +1,50 @@
-const regex = /[^a-z]/g;
+const notAllowed = /[^\p{Ll}]/gu; // Everything but lowercase letters
+
+let anagramSource;
+let anagramAttempt;
+let leftoverLetters;
+let leftoverLettersString;
 
 function go(){
-    document.getElementById("wrong").innerHTML = "";
-    let original = document.getElementById("original").value.toLowerCase().replaceAll(regex,""); // cristovarnovem
-    let anagram = document.getElementById("anagram").value.toLowerCase().replaceAll(regex,""); // timvan
-    let letters = original.split(""); // c,r,i,s,t,o,v,a,r,n,o,v,e,m
-    for (let i = 0; i < anagram.length; i++){
-        if (anagram[i] && letters.includes(anagram[i])){
-            for (let j = 0; j < letters.length; j++){
-                if (letters[j] == anagram[i]){
-                    letters.splice(j,1);
-                    j = letters.length;
+    resetErrorMessage();
+    getAnagramSource();
+    getAnagramAttempt();
+    generateLeftoverLetters();
+    displayLeftoverLetters(); // This also displays the error message; it would be better to separate them
+}
+
+function resetErrorMessage(){
+    document.getElementById("errorMessage").innerHTML = "";
+}
+
+function getAnagramSource(){
+    anagramSource = document.getElementById("anagramSource").value.toLowerCase().replaceAll(notAllowed,"");
+}
+function getAnagramAttempt(){
+    anagramAttempt = document.getElementById("anagramAttempt").value.toLowerCase().replaceAll(notAllowed,"");
+}
+
+function generateLeftoverLetters(){
+    leftoverLetters = anagramSource.split("");
+    for (let i = 0; i < anagramAttempt.length; i++){
+        if (anagramAttempt[i] && leftoverLetters.includes(anagramAttempt[i])){
+            for (let j = 0; j < leftoverLetters.length; j++){
+                if (leftoverLetters[j] == anagramAttempt[i]){
+                    leftoverLetters.splice(j,1);
+                    j = leftoverLetters.length;
                 }
             }
         } else {
-            document.getElementById("wrong").innerHTML = "Je hebt een fout gemaakt: verwijder een <b>" + anagram[i] + "</b>.";
+            document.getElementById("errorMessage").innerHTML = "Je hebt een fout gemaakt: verwijder een <b>" + anagramAttempt[i] + "</b>.";
         }
-    } // c,r,s,o,r,o,v,e
-    let display = letters.join(""); //crsorove
-    if (display == ""){
-        document.getElementById("letters").innerHTML = "&mdash;";
+    }
+}
+
+function displayLeftoverLetters(){
+    leftoverLettersString = leftoverLetters.join("");
+    if (leftoverLettersString == ""){
+        document.getElementById("leftoverLetters").innerHTML = "&mdash;";
     } else {
-        document.getElementById("letters").innerHTML = display;
+        document.getElementById("leftoverLetters").innerHTML = leftoverLettersString;
     }
 }
